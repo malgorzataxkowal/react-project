b6bb3527ab48f409afa6c86ed80d24e4cca1aea1
DIR\malgorzata.kowal <malgorzataxkowal@gmail.com>
2023-03-06 11:29:33 +0100

    Update mockDate- authorId field

--- tools/mockData.js
+++ tools/mockData.js
@@ -4,51 +4,61 @@ const shoes = [
     title: "newBalance",
     price: "30",
     category: "summer",
+    authorId: "2",
   },
   {
     id: 2,
     title: "Geox",
     category: "winter",
+    authorId: "2",
   },
   {
     id: 3,
     title: "newBalance",
     category: "winter",
+    authorId: "1",
   },
   {
     id: 4,
     title: "Geox",
     category: "spring",
+    authorId: "2",
   },
   {
     id: 5,
     title: "newBalance",
     category: "summer",
+    authorId: "2",
   },
   {
     id: 6,
     title: "newBalance",
     category: "spring",
+    authorId: "3",
   },
   {
     id: 7,
     title: "Geox",
     category: "summer",
+    authorId: "3",
   },
   {
     id: 8,
     title: "Geox",
     category: "autumn",
+    authorId: "1",
   },
   {
     id: 9,
     title: "Adidas",
     category: "autumn",
+    authorId: "1",
   },
   {
     id: 10,
     title: "Adidas",
     category: "summer",
+    authorId: "1",
   },
 ];
 
